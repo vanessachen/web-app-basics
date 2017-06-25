2e7d49edaf83de960468f2e859e80298f97cba57
Robyn Allen <enrobyn@users.noreply.github.com>
2017-06-25 16:38:56 -0400

    add vocab list for day 1

---
+++ day1vocabulary.py
@@ -0,0 +1,14 @@
+'''
+	day1vocabulary.py
+	Vocabulary list for Day 1: Data
+'''
+
+
+vocab_list = {
+	
+	"git init"	: "git command to initialize a git repository",
+	"import random"	: "Python command to import the random module",
+	"persistence"	: "the word programmers use to refer to the ability to store data which outlasts a single app session",
+	"shelve"		: "a Python module good for learning about databases",
+	"dictionary"	: "a Python data structure consisting of key-value pairs"
+}
