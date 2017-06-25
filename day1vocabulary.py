fa48ac0f2be0ef32079b41c0d33b962cf4f9e9be
Robyn Allen <enrobyn@users.noreply.github.com>
2017-06-25 19:30:41 -0400

    add vocab items

--- day1vocabulary.py
+++ day1vocabulary.py
@@ -1,14 +1,25 @@
 '''
 	day1vocabulary.py
-	Vocabulary list for Day 1: Data
+	Vocabulary for Day 1: Data
 '''
 
 
-vocab_list = {
+items_to_learn = {
 	
-	"git init"	: "git command to initialize a git repository",
+	"git init"		: "git command to initialize a git repository",
 	"import random"	: "Python command to import the random module",
 	"persistence"	: "the word programmers use to refer to the ability to store data which outlasts a single app session",
 	"shelve"		: "a Python module good for learning about databases",
 	"dictionary"	: "a Python data structure consisting of key-value pairs"
+	"bootstrap"		: "name of an open source project providing JavaScript and HTML components for web apps",
+	"asynchronous"	: "adjective describing software functions which can occur at any time without needing to wait for any previous function call to be completed",
+	"SQL"			: "The abbreviation for a database language standard called Structured Query Language",
+	"round trip"	: "A common English phrase which means 'to go somewhere and then return' and which, in computing, refers to sending a piece of information to a remote software process or database and also receiving a reply",
+	"localhost"		: "The host name for your local computer",
+	"virtualenv"	: "The one-word command used to create a Python virtual environment",
+	"repo"			: "The short nickname of a git repository",
+	"CRUD"			: "Acronym for the four main database operations: Create, Read, Update, Delete",
+	"pip"			: "3-letter command to install Python modules",
+	"deactivate"	: "Command to stop a virtual environment",
+	"activate"		: "Command to start a virtual environment",	
 }
