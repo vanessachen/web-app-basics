ea49d0287c609d6f115ff311e865d4965862a467
Robyn Allen <enrobyn@users.noreply.github.com>
2017-06-27 09:19:44 -0400

    fix typo

--- day1vocabulary.py
+++ day1vocabulary.py
@@ -10,7 +10,7 @@ items_to_learn = {
 	"import random"	: "Python command to import the random module",
 	"persistence"	: "the word programmers use to refer to the ability to store data which outlasts a single app session",
 	"shelve"		: "a Python module good for learning about databases",
-	"dictionary"	: "a Python data structure consisting of key-value pairs"
+	"dictionary"	: "a Python data structure consisting of key-value pairs",
 	"bootstrap"		: "name of an open source project providing JavaScript and HTML components for web apps",
 	"asynchronous"	: "adjective describing software functions which can occur at any time without needing to wait for any previous function call to be completed",
 	"SQL"			: "The abbreviation for a database language standard called Structured Query Language",
