db8dd3e4f2d7fd5ad6eaf53cb60ef1a363eef83d
Robyn Allen <enrobyn@users.noreply.github.com>
2017-06-28 09:22:17 -0400

    add vocab list for Day 3

---
+++ day3vocabulary.py
@@ -0,0 +1,28 @@
+'''
+	day3vocabulary.py
+	Vocabulary for Day 3: Sending data to the browser
+'''
+
+
+items_to_learn = {
+	
+	"jinja2"		: "the templating language used inside HTML templates for Flask",
+	"collection"	: "The word for a set of MongoDB documents",
+	"HTML"			: "The language of the web! Abbreviation for `HyperText Markup Language`",
+	"MongoDB"		: "A specific type of NoSQL database whose names comes from the word `humongous`",
+	"templates"		: "The name of the folder in which Flask expects to find HTML files",
+	"app.py"		: "The name of the main Python file for a Flask application",
+	"UX"			: "The common way to refer to the experience of the person using your app.  Short for `User eXperience`.",
+	"static"		: "The name of the folder in which Flask expects to find CSS- and JavaScript-related folders",
+	"github"		: "The name of a useful git website",
+	"git clone"		: "The git command to CLONE a repo",
+	"_id"			: "A unique value MongoDB assigns to each document automatically",
+	"git branch"	: "The git command to see all branches",
+	"git log"		: "The git command to view the LOG of recent changes",
+	"JSON"			: "A data-interchange format very common on the internet.  Abbreviation for `JavaScript Object Notation`",
+	"git status"	: "The git command to see the STATUS of the current repo",
+	"document"		: "The word for a single MongoDB data record",
+	"BSON"			: "The format MongoDB uses to encode data. A portmanteau of the words `binary` and `JSON`.",
+	"API"			: "A set of well-defined functions and protocols for writing software which talks to other software.  Abbreviation for `Application Program Interface`",
+	"UI"			: "Graphics, buttons, icons, etc. with which a person can interact with your app. Abbreviation for `User Interface`."
+}
