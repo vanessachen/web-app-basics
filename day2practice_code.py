3397aa6f058dde1e43675de4b9c23f930f613c67
vanessachen <vanchen@nuevaschool.org>
2018-01-10 15:22:15 -0800

    add to code to incorporate shelve

--- day2practice_code.py
+++ day2practice_code.py
@@ -13,7 +13,7 @@ import shelve
 study_items = shelve.open("items.db")
 item_results = shelve.open("results.db")
 
-# YOUR JOB: 
+# YOUR JOB:
 # take the items from items_to_learn and put them
 # into the shelve called study_items one at a time.
 
@@ -26,20 +26,31 @@ for item in items_to_learn.keys():
     print("******")
     print(definition)
 
-    # get input from the user and save it into 
+    # get input from the user and save it into
     # a variable called `guess` (which is always a STRING)
     guess = input("type your guess >  ")
 
 
     # check whether the user answered correctly
-
     # record the result into the item_results shelve
+    if guess == item:
+        print("You are correct")
+        item_results[item] = "correct"
+    elif guess != item:
+        print("Incorrect, this is the word: '{}'".format(item))
+        item_results[item] = "study more"
 
 
 # loop over the items in item_results
 # and figure out which ones the user needs to practice.
+for item in items_to_learn.keys():
+    if item_results[item]=="correct":
+        print("These are the terms you answered correctly: {}".format(item))
+    elif item_results[item]=="study more":
+        print("These are the terms you answered incorrectly: {}".format(item))
+        study_items[item] = item
 # Then display a summary to the user (using `print()` )
-
+print ("These are the items to study: {}".format(study_items))
 
 
 # don't forget to close the shelves!!
